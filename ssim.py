c2a3fb24d3b6abaefb3d9375bd84b1b3e03edea7
harnvo <wzc13162016886@163.com>
2022-09-19 12:08:52 +0800

    Add files via upload

--- ssim.py
+++ ssim.py
@@ -1,59 +1,64 @@
 import torch
 import torch.nn.functional as func
+from torch import nn
 
-def mySSIM(img, img2, **kwargs):
-    """
-    Ref:
-    Image quality assessment: From error visibility to structural similarity
+# most parameters in pytorch modules are torch.float32 by default
+_kernel = torch.tensor([[0.0010],
+                        [0.0076],
+                        [0.0360],
+                        [0.1094],
+                        [0.2130],
+                        [0.2660],
+                        [0.2130],
+                        [0.1094],
+                        [0.0360],
+                        [0.0076],
+                        [0.0010]], dtype=torch.float32)
+_window = torch.mm(_kernel, _kernel.T)
+_c1 = (0.01 * 255) ** 2
+_c2 = (0.03 * 255) ** 2
 
-    The results are the same as that of the official released MATLAB code in
-    https://ece.uwaterloo.ca/~z70wang/research/ssim/.
 
-    For three-channel images, SSIM is calculated for each channel and then
-    averaged.
+# def mySSIM(img, img2, **kwargs):
+#     """
+#     Ref:
+#     Image quality assessment: From error visibility to structural similarity
 
-    Args:
-        img (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
-        img2 (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
+#     The results are the same as that of the official released MATLAB code in
+#     https://ece.uwaterloo.ca/~z70wang/research/ssim/.
 
-    Returns:
-        float: SSIM result.
-    """
+#     For three-channel images, SSIM is calculated for each channel and then
+#     averaged.
 
-    assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
+#     Args:
+#         img (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
+#         img2 (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
 
-    img = img.to(torch.float64)
-    img2 = img2.to(torch.float64)
+#     Returns:
+#         float: SSIM result.
+#     """
 
-    ssim = _ssim(img * 255., img2 * 255.)
-    return ssim
+#     assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
+
+#     img = img.to(torch.float64)
+#     img2 = img2.to(torch.float64)
+
+#     ssim = _ssim(img * 255., img2 * 255.)
+#     return ssim
 
 def _ssim(img, img2):
     """
     Args:
-        img (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
-        img2 (Tensor): Images with range [0, 1], shape (n, 3/1, h, w).
+        img (Tensor): Images with range [0, 255], shape (n, 3/1, h, w).
+        img2 (Tensor): Images with range [0, 255], shape (n, 3/1, h, w).
 
     Returns:
         float: SSIM result.
     """
-    
-    c1 = (0.01 * 255) ** 2
-    c2 = (0.03 * 255) ** 2
 
     "Generated Gaussian Kernel, shape (11, 1.5)"
-    kernel = torch.tensor([[0.0010],
-                           [0.0076],
-                           [0.0360],
-                           [0.1094],
-                           [0.2130],
-                           [0.2660],
-                           [0.2130],
-                           [0.1094],
-                           [0.0360],
-                           [0.0076],
-                           [0.0010]], dtype=torch.float64)
-    window = torch.mm(kernel, kernel.T)
+
+    window = torch.mm(_kernel, _kernel.T)
     window = window.view(1, 1, 11, 11).expand(img.size(1), 1, 11, 11).to(img.dtype).to(img.device)
 
     mu1 = func.conv2d(img, window, stride=1, padding=0, groups=img.shape[1])
@@ -65,6 +70,41 @@ def _ssim(img, img2):
     sigma2_sq = func.conv2d(img2 * img2, window, stride=1, padding=0, groups=img.shape[1]) - mu2_sq
     sigma12 = func.conv2d(img * img2, window, stride=1, padding=0, groups=img.shape[1]) - mu1_mu2
 
-    cs_map = (2 * sigma12 + c2) / (sigma1_sq + sigma2_sq + c2)
-    ssim_map = ((2 * mu1_mu2 + c1) / (mu1_sq + mu2_sq + c1)) * cs_map
+    cs_map = (2 * sigma12 + _c2) / (sigma1_sq + sigma2_sq + _c2)
+    ssim_map = ((2 * mu1_mu2 + _c1) / (mu1_sq + mu2_sq + _c1)) * cs_map
     return ssim_map.mean([1, 2, 3])
+
+class SSIMLoss(nn.Module):
+    def __init__(self, dtype=torch.float32):
+        super(SSIMLoss, self).__init__()
+        self.ssim = _ssim
+        # register kernel as buffer
+        self.register_buffer( 'window', _window.view(1, 1, 11, 11).expand(1,1,11,11).to(dtype) )
+
+    def _ssim(self, img, img2):
+        """
+        Args:
+            img (Tensor): Images with range [0, 255], shape (n, 3/1, h, w).
+            img2 (Tensor): Images with range [0, 255], shape (n, 3/1, h, w).
+
+        Returns:
+            float: SSIM result.
+        """
+        window = window.expand(img.size(1)).to(img.device)
+
+        mu1 = func.conv2d(img, window, stride=1, padding=0, groups=img.shape[1])
+        mu2 = func.conv2d(img2, window, stride=1, padding=0, groups=img2.shape[1])
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+        sigma1_sq = func.conv2d(img * img, window, stride=1, padding=0, groups=img.shape[1]) - mu1_sq
+        sigma2_sq = func.conv2d(img2 * img2, window, stride=1, padding=0, groups=img.shape[1]) - mu2_sq
+        sigma12 = func.conv2d(img * img2, window, stride=1, padding=0, groups=img.shape[1]) - mu1_mu2
+
+        cs_map = (2 * sigma12 + _c2) / (sigma1_sq + sigma2_sq + _c2)
+        ssim_map = ((2 * mu1_mu2 + _c1) / (mu1_sq + mu2_sq + _c1)) * cs_map
+        return ssim_map.mean([1, 2, 3])
+
+    def forward(self, img, img2):
+        assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
+        return 1 - self.ssim(img, img2)
--- test.py
+++ test.py
@@ -3,11 +3,13 @@ import ssim
 import numpy as np
 from torch.utils.data import DataLoader
 from torchvision.io import read_image
-from ssim import mySSIM
+from ssim import SSIMLoss
 from skimage.metrics import structural_similarity as skSSIM
 
 def tensor2numpy(img):
     return np.array(img.cpu().detach())
+
+mySSIM = SSIMLoss()
 
 # Test Bench #1
 print("Test #1")
@@ -15,41 +17,42 @@ print("Calculate  Skimage    Benchmark")
 
 benchmark = [1.000, 0.988, 0.913, 0.840, 0.694, 0.662]
 
-originalImg = read_image("data\image4.jpg") / 255.
+originalImg = read_image("data\image4.jpg").to(torch.float32)
 inputImg = originalImg.unsqueeze(0)
+print(originalImg.size(1))
 
 for i in range(6):
-    testImg = read_image("data\image{}.jpg".format(4+2*i)) / 255.
+    testImg = read_image("data\image{}.jpg".format(4+2*i)).to(torch.float32)
     outputImg = testImg.unsqueeze(0)
     calcSSIM = mySSIM(inputImg, outputImg).item()
     testSSIM = skSSIM(tensor2numpy(originalImg), tensor2numpy(testImg), channel_axis=0)
-    print("{:.3f}      {:.3f}      {:.3f}".format(calcSSIM, testSSIM, benchmark[i]))
+    print("{:.3f}      {:.3f}      {:.3f}".format(1-calcSSIM, testSSIM, benchmark[i]))
 
 print()
 # Test Bench #2
 print("Test #2")
 print("Calculate  Skimage    Benchmark")
 
-originalImg = read_image("data\simga_0_ssim_1.0000.png") / 255.
+originalImg = read_image("data\simga_0_ssim_1.0000.png").to(torch.float64)
 inputImg = originalImg.unsqueeze(0)
 
-testImg = read_image("data\simga_0_ssim_1.0000.png") / 255.
+testImg = read_image("data\simga_0_ssim_1.0000.png").to(torch.float64)
 outputImg = testImg.unsqueeze(0)
 
 calcSSIM = mySSIM(inputImg, outputImg).item()
 testSSIM = skSSIM(tensor2numpy(originalImg), tensor2numpy(testImg), channel_axis=0)
-print("{:.4f}     {:.4f}     1.0000".format(calcSSIM, testSSIM))
+print("{:.4f}     {:.4f}     1.0000".format(1-calcSSIM, testSSIM))
 
-testImg = read_image("data\simga_50_ssim_0.4225.png") / 255.
+testImg = read_image("data\simga_50_ssim_0.4225.png").to(torch.float64)
 outputImg = testImg.unsqueeze(0)
 
 calcSSIM = mySSIM(inputImg, outputImg).item()
 testSSIM = skSSIM(tensor2numpy(originalImg), tensor2numpy(testImg), channel_axis=0)
-print("{:.4f}     {:.4f}     0.4225".format(calcSSIM, testSSIM))
+print("{:.4f}     {:.4f}     0.4225".format(1-calcSSIM, testSSIM))
 
-testImg = read_image("data\simga_100_ssim_0.1924.png") / 255.
+testImg = read_image("data\simga_100_ssim_0.1924.png").to(torch.float64)
 outputImg = testImg.unsqueeze(0)
 
 calcSSIM = mySSIM(inputImg, outputImg).item()
 testSSIM = skSSIM(tensor2numpy(originalImg), tensor2numpy(testImg), channel_axis=0)
-print("{:.4f}     {:.4f}     0.1924".format(calcSSIM, testSSIM))
+print("{:.4f}     {:.4f}     0.1924".format(1-calcSSIM, testSSIM))
